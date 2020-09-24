6700219114f0e80bf01a8541b7d18a65913bff02
Claire Wong <claire2017@gmail.com>
2020-09-23 17:32:09 -0700

    added JSoup library for the on this day api

--- src/main/java/com/mcjag/daytoday/DayToDayApplication.java
+++ src/main/java/com/mcjag/daytoday/DayToDayApplication.java
@@ -1,10 +1,16 @@
 package com.mcjag.daytoday;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 @RestController
@@ -25,8 +31,10 @@ public class DayToDayApplication {
 	}
 
 	@GetMapping("/onThisDay")
-	public String onThisDay() {
-		return "On this day...";
+	public String onThisDay() throws IOException {
+		Document doc = Jsoup.connect("https://www.history.com/this-day-in-history").get();
+		String keywords = doc.select("meta[itemprop=description]").first().attr("content");
+		return String.format("On this day: %s", keywords);
 	}
 	
 	@GetMapping("/tomorrow")
