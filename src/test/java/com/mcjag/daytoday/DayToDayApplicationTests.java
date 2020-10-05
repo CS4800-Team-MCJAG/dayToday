fc4eb71b17d98bc4b4f5db46a1cd38de7f03dcff
Michelle Lam <michelleelamm@users.noreply.github.com>
2020-10-04 20:50:54 -0700

    Test Tomorrow case

--- src/test/java/com/mcjag/daytoday/DayToDayApplicationTests.java
+++ src/test/java/com/mcjag/daytoday/DayToDayApplicationTests.java
@@ -3,9 +3,12 @@ package com.mcjag.daytoday;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import jdk.jfr.Timestamp;
 
 import java.io.IOException;
 
@@ -29,6 +32,11 @@ class DayToDayApplicationTests {
 	void test() throws IOException {
 		Boolean res = this.controller.onThisDay().contains("On this day: ");
 		Assert.assertTrue(res);
+	}
+
+	@Test
+	void testTomorrow(){
+		assertThat(this.controller.tomorrow()).contains("Tomorrow");
 	}
 
 }
