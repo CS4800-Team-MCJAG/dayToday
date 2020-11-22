fccb6ca371f64ee2e531a8e3634092440495745d
Michelle Lam <michelleelamm@users.noreply.github.com>
2020-11-22 15:05:54 -0800

    undo errors

--- src/test/java/com/mcjag/daytoday/DayToDayApplicationTests.java
+++ src/test/java/com/mcjag/daytoday/DayToDayApplicationTests.java
@@ -7,18 +7,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import java.io.IOException;
 
 @SpringBootTest
-@RunWith(SpringJUnit4ClassRunner.class)
-
 class DayToDayApplicationTests {
 
+	@Autowired
+	private WelcomeProvider welcomeProvider;
 	@Test
 	void contextLoads() {
 	}
